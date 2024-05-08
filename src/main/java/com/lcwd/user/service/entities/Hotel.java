c7e5cb83670c88e1f9a364f3ff8e657fe12a1acc
Ajethero <gowthammeka1@gmail.com>
2024-05-09 00:01:29 +0530

    added project

---
+++ src/main/java/com/lcwd/user/service/entities/Hotel.java
@@ -0,0 +1,41 @@
+package com.lcwd.user.service.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+public class Hotel {
+
+    private  String id;
+    private  String name;
+    private  String location;
+    private  String about;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAbout() {
+		return about;
+	}
+	public void setAbout(String about) {
+		this.about = about;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+}
